febfd2ccd195180077629a1e0bc27227290cef67
Rajeev Koushik Y G <rajeevkoushikyg@gmail.com>
2024-01-07 19:34:06 +0530

    Minor changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,22 @@ const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
+// signup user
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 // login user
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
@@ -21,23 +37,7 @@ const loginUser = async (req, res) => {
   }
 };
 
-// signup use
-const signupUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.signup(email, password);
-
-    // create token
-    const token = createToken(user._id);
-
-    res.status(200).json({ email, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
 module.exports = {
-  loginUser,
   signupUser,
+  loginUser,
 };
